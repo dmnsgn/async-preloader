88cc06accff789f31719e9fa2fe65215b832263e
Damien Seguin <dmnsgn@gmail.com>
2024-07-08 13:25:43 +0100

    build: update dependencies

--- test/browser.spec.js
+++ test/browser.spec.js
@@ -26,17 +26,26 @@ describe("Browser", () => {
     await page.goto(url);
 
     // page
-    //   .on("console", (message) =>
+    //   .on("console", async (message) => {
+    //     if (message.text() !== "JSHandle@error") {
+    //       console.log(
+    //         `${message.type().substring(0, 3).toUpperCase()} ${message.text()}`,
+    //       );
+    //       return;
+    //     }
+    //     const messages = await Promise.all(
+    //       message.args().map((arg) => arg.getProperty("message")),
+    //     );
     //     console.log(
-    //       `${message.type().substr(0, 3).toUpperCase()} ${message.text()}`
-    //     )
-    //   )
+    //       `${message.type().substr(0, 3).toUpperCase()} error ${messages.filter(Boolean)}`,
+    //     );
+    //   })
     //   .on("pageerror", ({ message }) => console.log(message))
     //   .on("response", (response) =>
-    //     console.log(`${response.status()} ${response.url()}`)
+    //     console.log(`${response.status()} ${response.url()}`),
     //   )
     //   .on("requestfailed", (request) =>
-    //     console.log(`${request.failure().errorText} ${request.url()}`)
+    //     console.log(`${request.failure().errorText} ${request.url()}`),
     //   );
   });
 
@@ -54,7 +63,7 @@ describe("Browser", () => {
       {
         includeHostname: false,
         storagePath: "./.nyc_output",
-      }
+      },
     );
   });
 
@@ -231,7 +240,7 @@ describe("Browser", () => {
           family: data.family,
         };
       },
-      { src: items.get("font").src }
+      { src: items.get("font").src },
     );
 
     expect(result.constructor).toEqual("FontFace");
@@ -256,9 +265,9 @@ describe("Browser", () => {
     const item = items.get("fontface");
     await page.addStyleTag({
       content: /* css */ `@font-face {
-  font-family: '${item.id}';
-  src: url(${items.get("font").src}) format('truetype');
-}`,
+    font-family: '${item.id}';
+    src: url(${items.get("font").src}) format('truetype');
+  }`,
     });
 
     const result = await page.evaluate(async (item) => {
@@ -296,33 +305,44 @@ describe("Browser", () => {
     // );
   });
 
-  it("should cancel loading an array of LoadItem and return an AbortError", async () => {
+  it("should cancel loading an array of LoadItem and return an CancelLoading", async () => {
     let itemsToLoad = Array.from(items.values());
 
     const result = await page.evaluate(async (items) => {
       const { default: Preloader } = await importShim("./lib/index.js");
+
       const controller = new AbortController();
+      const { signal } = controller;
 
-      const timeoutId = setTimeout(() => {
-        controller.abort();
-      }, 0);
+      const load = async () => {
+        try {
+          await Preloader.loadItems(
+            items.map((item) => ({
+              ...item,
+              options: {
+                ...(item.options || {}),
+                signal,
+              },
+            })),
+          );
+        } catch (error) {
+          return error;
+        }
+        return "NoError";
+      };
 
-      try {
-        await Preloader.loadItems(
-          items.map((item) => ({
-            ...item,
-            options: { ...(item.options || {}), signal: controller.signal },
-          }))
-        );
-      } catch (error) {
-        return error.name;
-      } finally {
-        clearTimeout(timeoutId);
-      }
+      const results = await Promise.allSettled([
+        (async () => {
+          controller.abort("CancelLoading");
+        })(),
+        load(),
+      ]);
 
-      return "NoError";
+      return results[1].value;
     }, itemsToLoad);
 
-    expect(result).toEqual("AbortError");
+    console.log("result", result);
+
+    expect(result).toEqual("CancelLoading");
   });
 });
